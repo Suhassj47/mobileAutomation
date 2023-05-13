bfa40abf9fec3e04df3521fef86287be641c757b
Suhassj47 <133471080+Suhassj47@users.noreply.github.com>
2023-05-13 09:16:08 +0530

    Suhas MobileAutomation

---
+++ src/practiceSuhas/test.java
@@ -0,0 +1,68 @@
+package practiceSuhas;
+import org.testng.annotations.Test;
+import org.testng.AssertJUnit;
+import org.testng.annotations.Test;
+import org.testng.AssertJUnit;
+import org.testng.annotations.Test;
+import org.testng.AssertJUnit;
+import org.testng.annotations.Test;
+import org.testng.AssertJUnit;
+import org.testng.annotations.Test;
+import org.testng.AssertJUnit;
+import org.openqa.selenium.By;
+import practiceSuhas.TestUtils.AndroidBaseTest;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import io.appium.java_client.AppiumBy;
+import io.appium.java_client.android.Activity;
+
+public class test extends AndroidBaseTest{
+
+	
+	
+	@BeforeMethod
+	public void preSetup()
+	{
+		//screen to home page
+		Activity activity = new Activity("com.androidsample.generalstore", "com.androidsample.generalstore.MainActivity");
+		driver.startActivity(activity);
+		
+		
+	}
+	
+	@Test
+
+	public void Login()
+	{
+		
+		driver.findElement(By.id("com.androidsample.generalstore:id/nameField")).sendKeys("Suhas");
+		driver.hideKeyboard();
+		driver.findElement(By.xpath("//android.widget.RadioButton[@text='Male']")).click();
+		driver.findElement(By.id("android:id/text1")).click();
+		driver.findElement(By.id("com.androidsample.generalstore:id/btnLetsShop")).click();
+		String toastMessage = driver.findElement(By.xpath("(//android.widget.Toast)[1]")).getAttribute("name");
+		AssertJUnit.assertEquals(toastMessage,"Please your name");
+		
+		
+			
+	}
+	
+
+	@Test
+	public void FailedLogin()
+
+	
+	{
+		
+		driver.findElement(By.id("com.androidsample.generalstore:id/nameField")).sendKeys("Tony");
+		driver.hideKeyboard();
+		driver.findElement(By.xpath("//android.widget.RadioButton[@text='Female']")).click();
+		driver.findElement(By.id("android:id/text1")).click();
+		driver.findElement(By.id("com.androidsample.generalstore:id/btnLetsShop")).click();
+		String toastMessage = driver.findElement(By.xpath("(//android.widget.Toast)[1]")).getAttribute("name");
+		AssertJUnit.assertEquals(toastMessage,"Please your name");
+		
+			
+	}
+}
